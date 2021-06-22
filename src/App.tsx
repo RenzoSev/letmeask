e56ecc11403748ab97e4ef1d8fe04da8c993be91
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-21 21:39:56 -0300

    feat: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Route, Switch } from 'react-router-dom';
+
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
 import { ThemeProvider } from 'styled-components';
@@ -11,7 +13,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <NewRoom />
+      
+      <Switch>
+        <Route path="/rooms/new" component={NewRoom}/>
+        <Route path="/" component={Home}/>
+      </Switch>
     </ThemeProvider>
   );
 }
