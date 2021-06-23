866721f5a10a10d6e63337967e777bae00f79d56
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-22 22:01:09 -0300

    feat: styles room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,40 @@
+import Button from '../components/Button';
+import logoImg from '../assets/images/logo.svg';
+import {
+  PageRoom,
+  DivContent,
+  MainContent,
+  RoomTitle,
+  FormFooter,
+} from '../styles/room';
+
 export function Room() {
   return (
-    <h1>Oi!</h1>
-  )
+    <PageRoom>
+      <header>
+        <DivContent>
+          <img src={logoImg} alt="Letmeask" />
+          <div>codigo</div>
+        </DivContent>
+      </header>
+
+      <MainContent>
+        <RoomTitle>
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </RoomTitle>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+
+          <FormFooter>
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>.
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </FormFooter>
+        </form>
+      </MainContent>
+    </PageRoom>
+  );
 }
