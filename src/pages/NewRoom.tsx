490c3cc9cd500eac4ecc63d6eeee08c671648257
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-22 21:06:39 -0300

    feat: handleCreateRoom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../components/Button';
@@ -6,28 +7,50 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { PageAuth, MainContent } from '../styles/auth';
 import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 function NewRoom() {
-  const { user, signInWithGoogle } = useAuth();
-  
+  const { user } = useAuth();
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+  }
+
   return (
     <PageAuth>
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
+
       <main>
         <MainContent>
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input 
+          <form onSubmit={handleCreateRoom}>
+            <input
               placeholder="Nome da sala"
+              onChange={(e) => setNewRoom(e.target.value)}
+              value={newRoom}
             />
-            <Button type="submit">
-              Criar sala
-            </Button>
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
             Quer entar em uma sala existente? <Link to="/">Clique aqui</Link>
