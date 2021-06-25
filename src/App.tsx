a741778ab983dfe6b641fbd410e2bd8de38db12f
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-25 12:53:25 -0300

    fix: eslint done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Route, Switch } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
@@ -19,7 +17,6 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-
       <AuthContextProvider>
         <Switch>
           <Route path="/rooms/new" component={NewRoom} />
