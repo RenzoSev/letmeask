eaa57b44cd3b8836842f7878dc52660715e930e0
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-26 12:49:13 -0300

    PWA version done

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './services/firebase';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 
@@ -16,5 +15,3 @@ ReactDOM.render(
 );
 
 serviceWorkerRegistration.register();
-
-reportWebVitals();
